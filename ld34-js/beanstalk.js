a7782bd805ba412afa19da5e9dcf7151e9ae5067
Duncan Holter <djholter@ncsu.edu>
2015-12-13 14:58:10 -0500

    barely any bean

---
+++ ld34-js/beanstalk.js
@@ -0,0 +1,20 @@
+function Beanstalk(){
+    this.segmentCount = 6;
+    this.segmentsExtruded = 0;
+    this.z = 3;
+
+    this.segments = [];
+
+    this.update()
+}
+Beanstalk.prototype = new Located();
+
+function BeanTrunk(){
+    this.percentExtruded = 0;
+    
+}
+BeanTrunk.prototype = new Drawable()
+function BeanLeaf(){
+
+}
+BeanLeaf.prototype = new Animation();
